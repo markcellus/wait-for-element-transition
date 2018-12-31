34dec3abf04c712ea10c102e469b1eb83bedd767
Mark <mark@heyimmark.com>
2018-12-31 17:16:31 -0500

    3.0.0

---
+++ dist/wait-for-element-transition.d.ts
@@ -0,0 +1,4 @@
+/**
+ * Builds a transition promise that waits to resolve until the module el's CSS transition is completed (if applicable).
+ */
+export default function waitForElementTransition(el: HTMLElement): Promise<HTMLElement>;
