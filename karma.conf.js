1c524a96ba959727772dda1dcd3811bd616bbcbd
Mark <1446916+markcellus@users.noreply.github.com>
2021-03-13 09:41:12 -0500

    Add cross-browser testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const tsconfig = require('./tsconfig.json');
 
 module.exports = function (config) {
@@ -15,12 +17,49 @@ module.exports = function (config) {
             },
             exclude: ['node_modules'],
         },
+        browserStack: {
+            username: process.env.BROWSERSTACK_USERNAME,
+            accessKey: process.env.BROWSERSTACK_ACCESS_KEY,
+        },
+        customLaunchers: {
+            chrome: {
+                base: 'BrowserStack',
+                browser: 'chrome',
+                os: 'OS X',
+                os_version: 'Big Sur',
+            },
+            safari: {
+                base: 'BrowserStack',
+                os: 'OS X',
+                os_version: 'Big Sur',
+                browser: 'safari',
+            },
+            edge: {
+                base: 'BrowserStack',
+                os: 'Windows',
+                os_version: '10',
+                browser: 'edge',
+            },
+            firefox: {
+                base: 'BrowserStack',
+                os: 'Windows',
+                os_version: '10',
+                browser: 'firefox',
+            },
+            ios: {
+                base: 'BrowserStack',
+                device: 'iPhone X',
+                os: 'ios',
+                real_mobile: true,
+                os_version: '11.0',
+            },
+        },
+        browsers: ['chrome', 'firefox', 'safari', 'edge', 'ios'],
         reporters: ['progress'],
         frameworks: ['mocha', 'karma-typescript'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
-        browsers: ['ChromeHeadless'],
         autoWatch: true,
         singleRun: true,
         concurrency: Infinity,
