6dd66299979dfa3512cd1438c992fde26859a2aa
Mark <1446916+markcellus@users.noreply.github.com>
2021-03-14 10:00:41 -0400

    Remove dist files from version control

--- dist/wait-for-element-transition.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Builds a transition promise that waits to resolve until the module el's CSS transition is completed (if applicable).
- */
-export default function waitForElementTransition(el: HTMLElement): Promise<HTMLElement>;
